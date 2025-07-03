9960b45ae0fa568dafdb7556e26f5d9e4d0291f7
Hasegos <csw020106@naver.com>
2025-07-03 16:43:40 +0900

    test

---
+++ src/test/java/oreumi/group2/carrotClone/CarrotCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package oreumi.group2.carrotClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrotCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
