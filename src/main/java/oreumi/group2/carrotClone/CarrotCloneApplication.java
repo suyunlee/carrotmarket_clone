6a0984192ed769c78ae4779520ee19edec15ce3f
Hasegos <csw020106@naver.com>
2025-07-03 16:44:27 +0900

    add application

---
+++ src/main/java/oreumi/group2/carrotClone/CarrotCloneApplication.java
@@ -0,0 +1,13 @@
+package oreumi.group2.carrotClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrotCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrotCloneApplication.class, args);
+	}
+
+}
