24e7eb231e5ef3ad92e5fdc56c129ae58731b872
Hasegos <csw020106@naver.com>
2025-07-04 10:49:37 +0900

    add ChatRoomEntity

--- src/main/java/oreumi/group2/carrotClone/model/ChatRoom.java
+++ src/main/java/oreumi/group2/carrotClone/model/ChatRoom.java
@@ -8,6 +8,9 @@ import lombok.NoArgsConstructor;
 import java.util.ArrayList;
 import java.util.List;
 
+
+@Entity
+@Table(name = "chat_room")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -17,16 +20,17 @@ public class ChatRoom {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL)
-    private List<ChatMessage> messages = new ArrayList<>();
-
-    @ManyToOne(optional = false)
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id", nullable = false)
     private Post post;
 
-    @ManyToOne(optional = false)
+    @ManyToOne(optional = false,fetch =  FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    @OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ChatMessage> messages = new ArrayList<>();
+
+    @Column(name = "ischatbot", nullable = false)
     private boolean isChatBot;
 }
