f0bdec63f5c73a2b9fe0c39df2bbabcc9315b978
Hasegos <csw020106@naver.com>
2025-07-04 10:49:27 +0900

    add ImageEntity

---
+++ src/main/java/oreumi/group2/carrotClone/model/Image.java
@@ -0,0 +1,25 @@
+package oreumi.group2.carrotClone.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "image")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "post_id",nullable = false)
+    private Post post; /* post 정보 */
+
+    @Column(nullable = false)
+    private String  imageUrl; /* 이미지 url */
+}
