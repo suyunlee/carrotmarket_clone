511facb2a043f0529fa26fbbfa9f2e15e7636b9a
Hasegos <csw020106@naver.com>
2025-07-04 16:38:38 +0900

    add UserService interface

---
+++ src/main/java/oreumi/group2/carrotClone/service/UserService.java
@@ -0,0 +1,37 @@
+package oreumi.group2.carrotClone.service;
+
+import oreumi.group2.carrotClone.model.User;
+import oreumi.group2.carrotClone.model.enums.AuthProvider;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService {
+
+    /* 회원 가입*/
+    User register(User user);
+    
+    /* username 조회*/
+    Optional<User> findByUsername(String username);
+    
+    /* nickname 조회 */
+    Optional<User> findByNickname(String nickname);
+
+    /* id 기준으로 user 찾기 */
+    Optional<User> getUserById(Long id);
+
+    /* user 정보 업데이트 */
+    User updateUser(User user);
+
+    /* user 정보 삭제 */
+    void deleteUser(Long id);
+    
+    /* 휴대폰 전화 중복 확인 */
+    boolean existsByPhoneNumber(String phoneNumber);
+
+    /* 모든 유저 가져오기 */
+    List<User> getAllUsers();
+
+    /* 소셜 로그인 */
+    Optional<User> findByProviderAndProviderId(AuthProvider provider, String providerId);
+}
