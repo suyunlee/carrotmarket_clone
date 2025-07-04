e498884be403cc4ea99658430887b44e3f875c18
Hasegos <csw020106@naver.com>
2025-07-04 10:49:46 +0900

    add ChatMessageEntity

--- src/main/java/oreumi/group2/carrotClone/model/ChatMessage.java
+++ src/main/java/oreumi/group2/carrotClone/model/ChatMessage.java
@@ -4,9 +4,12 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "chat_message")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -16,14 +19,18 @@ public class ChatMessage {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(optional = false)
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
     @JoinColumn(name = "chatRoom_id", nullable = false)
     private ChatRoom chatRoom;
 
-    @ManyToOne(optional = false)
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
     @JoinColumn(name = "sender_id", nullable = false)
     private User sender;
 
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String content;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false, name = "created_at")
     private LocalDateTime createdAt;
 }
