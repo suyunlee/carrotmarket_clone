92e5341c3a849a156cf97a46d75c064385151284
Hasegos <csw020106@naver.com>
2025-07-07 14:27:02 +0900

    modify PostService

--- src/main/java/oreumi/group2/carrotClone/service/PostService.java
+++ src/main/java/oreumi/group2/carrotClone/service/PostService.java
@@ -1,16 +1,23 @@
 package oreumi.group2.carrotClone.service;
 
+import oreumi.group2.carrotClone.DTO.PostDTO;
 import oreumi.group2.carrotClone.model.Post;
+import oreumi.group2.carrotClone.model.User;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 public interface PostService {
-    List<Post> getAllPosts();
-    List<Post> searchPostsByTitle (String keyword);
-    List<Post> searchPostsByLocation (String location);
-    List<Post> getPostsSortedByNewest ();
-    List<Post> getPostsSortedByPrice (boolean ascending);
-    Post getPostById (Long id);
-    Post createPost (Post post);
-
+    List<Post> getAllPosts();  /* 전체 조회 */
+    List<Post> searchPostsByTitle (String keyword); /* 제목 조회 */
+    List<Post> searchPostsByLocation (String location); /* 위치기반 조회 */
+    List<Post> getPostsSortedByNewest (); /* 최신순  */
+    List<Post> getPostsSortedByPrice (boolean ascending); /* 가격순 */
+    Post getPostById (Long id); /* Id 기반 조회 */
+    Post createPost (String title, String description, BigDecimal price,
+                     String location,List<String> images); /* 저장 */
+    void deletePost(Long id); // 삭제
+    Post updatePost(Post post); /* 단일 업데이트 */
+    boolean isLikedByUser(Long postId, User user);
 }
