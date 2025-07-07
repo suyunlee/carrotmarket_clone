db99440c4378b613d6acf882d4ad2acaa289a747
Hasegos <csw020106@naver.com>
2025-07-07 14:26:55 +0900

    modify PostRepository

--- src/main/java/oreumi/group2/carrotClone/repository/PostRepository.java
+++ src/main/java/oreumi/group2/carrotClone/repository/PostRepository.java
@@ -8,9 +8,9 @@ import java.util.List;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
     //제목 기반 검색
-    List<Post> findbyTitleContainingIgnoreCase (String keyword);
+    List<Post> findByTitleContainingIgnoreCase (String keyword);
     // 장소 기반 검색
-    List<Post> findbyLocationContainingIgnorecase (String location);
+    List<Post> findByLocationContainingIgnoreCase (String location);
 
     List<Post> findAllByOrderByCreatedAtDesc(); //최신순 정렬
     List<Post> findAllByOrderByPriceAsc(); // 가격 낮은 순 정렬
