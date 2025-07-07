ccc9714f372d4c408ca11b6478d5789cfecfa869
Hasegos <csw020106@naver.com>
2025-07-07 14:26:44 +0900

    modify CategoryRepository

---
+++ src/main/java/oreumi/group2/carrotClone/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package oreumi.group2.carrotClone.repository;
+
+import oreumi.group2.carrotClone.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
