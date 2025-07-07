4dbe107d806108f683627f1d598ce3ba75c7d9c7
Hasegos <csw020106@naver.com>
2025-07-07 14:25:02 +0900

    add ImageRepository

---
+++ src/main/java/oreumi/group2/carrotClone/repository/ImageRepository.java
@@ -0,0 +1,7 @@
+package oreumi.group2.carrotClone.repository;
+
+import oreumi.group2.carrotClone.model.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image,Long> {
+}
