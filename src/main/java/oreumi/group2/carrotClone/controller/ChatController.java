2268e15a1d7d3c0a1fea444eeac0b8e663027b89
Hasegos <csw020106@naver.com>
2025-07-08 16:33:01 +0900

    modify ChatController (채팅방 post,get, message 추가)

--- src/main/java/oreumi/group2/carrotClone/controller/ChatController.java
+++ src/main/java/oreumi/group2/carrotClone/controller/ChatController.java
@@ -1,20 +1,76 @@
 package oreumi.group2.carrotClone.controller;
 
+import lombok.RequiredArgsConstructor;
+import oreumi.group2.carrotClone.DTO.ChatMessageDTO;
+import oreumi.group2.carrotClone.model.ChatMessage;
+import oreumi.group2.carrotClone.service.ChatMessageService;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-@Controller("chat")
+import java.util.List;
+
+@Controller
+@RequestMapping("/chat/room/{roomId}")
+@RequiredArgsConstructor
 public class ChatController {
 
-    @GetMapping("/{roomId}")
-    public String showChat(@RequestParam Long roomId){
-        return "";
+    private final ChatMessageService chatMessageService;
+    private final SimpMessagingTemplate template;
+
+    /* 채팅방 입장 */
+    @GetMapping
+    public String enterChat(
+            @PathVariable Long roomId,
+            @RequestParam String username,
+            /* Principal principal <- 로그인시 필요 */
+            Model model)
+    {
+        // String username = principal.getName();
+        model.addAttribute("roomId", roomId);
+        model.addAttribute("currentUser",username);
+
+        List<ChatMessageDTO> dtos = chatMessageService.getMessages(roomId)
+                        .stream()
+                        .map(ChatMessageDTO :: fromEntity)
+                        .toList();
+
+        model.addAttribute("messages",dtos );
+        return "chat-room";
     }
 
-    @PostMapping("/{roomId}/send")
-    public String postChat(@RequestParam Long roomId){
-        return "";
+    // 클라이언트가 이 엔드포인트를 구독하면 과거 메시지 전체를 반환
+    @GetMapping("/messages")
+    @ResponseBody
+    public List<ChatMessageDTO> getHistory(@PathVariable Long roomId) {
+        return chatMessageService.getMessages(roomId)
+                .stream()
+                .map(ChatMessageDTO :: fromEntity) // 만들어둔 Entity 랑 비교해서 필터링
+                .toList();
+    }
+
+    @MessageMapping("/room/{roomId}/send")
+    public void stompMessage(
+            @DestinationVariable Long roomId,
+            // @Payload 역직렬화 (JSON -> 객체)
+            @Payload ChatMessageDTO payload
+            /* Principal principal <- 로그인시 필요 */ )
+    {
+        // String username = principal.getName(); 로그인된 유저
+        /* 메세지 저장 (방 ID, 내용, 보낸 사람아름) */
+        ChatMessage saved = chatMessageService.saveMessage(
+                roomId,
+                payload.getContent(),
+                payload.getSenderUsername()
+                // username
+        );
+        // DTO 로 변환 (필터링)
+        ChatMessageDTO dto = ChatMessageDTO.fromEntity(saved);
+        // 같은 방 (topic) 구독자에게 메세지 전달
+        template.convertAndSend("/topic/chat/" + roomId,dto);
     }
 }
