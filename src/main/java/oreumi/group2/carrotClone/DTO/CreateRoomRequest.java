80ea6eaf512aaecf44ac65e1f6ae79d7045b8b29
Hasegos <csw020106@naver.com>
2025-07-08 16:34:06 +0900

    add CreateRoomRequest 추가

---
+++ src/main/java/oreumi/group2/carrotClone/DTO/CreateRoomRequest.java
@@ -0,0 +1,12 @@
+package oreumi.group2.carrotClone.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateRoomRequest {
+    private String username;
+}
