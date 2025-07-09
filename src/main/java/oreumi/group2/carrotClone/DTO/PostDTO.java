cf88a20ba2ff73c05460e0cbb22e2dbc00db7bf6
SuyunLee <dufrhdsy@naver.com>
2025-07-09 20:07:32 +0900

    modify postDTO vaild 추가

--- src/main/java/oreumi/group2/carrotClone/DTO/PostDTO.java
+++ src/main/java/oreumi/group2/carrotClone/DTO/PostDTO.java
@@ -1,14 +1,13 @@
 package oreumi.group2.carrotClone.DTO;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import oreumi.group2.carrotClone.model.Category;
-import oreumi.group2.carrotClone.model.Image;
-import oreumi.group2.carrotClone.model.Post;
 
-import javax.swing.*;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -23,13 +22,19 @@ public class PostDTO  implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Long id;    // 게시물 ID
+
+    @NotBlank(message = "제목을 입력하세요.")
     private String title;   // 게시글 제목
+    @NotBlank(message = "설명을 입력하세요.")
     private String description; // 내용
+    @NotNull(message = "가격을 입력하세요.")
     private BigDecimal price;   // 판매 가격
     private boolean sold;  // 판매 여부
     private LocalDate createdAt;    //등록 날짜
+    @NotBlank(message = "위치를 입력하세요.")
     private String location;    // 글 등록 위치
     private Integer viewCount;  // 조회수
     private Long userId;    // 게시글 작성 유저 아이디
     private List<String> images;
+    private Category category;
 }
