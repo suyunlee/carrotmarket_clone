b05752164d209e9eb169cff78a0d8d2557776f6d
Nyppp <wlsdnrla216@naver.com>
2025-07-11 15:40:35 +0900

    add CustomUserPrincipal

---
+++ src/main/java/oreumi/group2/carrotClone/Config/CustomUserPrincipal.java
@@ -0,0 +1,42 @@
+package oreumi.group2.carrotClone.Config;
+
+import oreumi.group2.carrotClone.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserPrincipal implements UserDetails {
+
+    private final User user;
+
+    public CustomUserPrincipal(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override public boolean isAccountNonExpired() { return true; }
+    @Override public boolean isAccountNonLocked() { return true; }
+    @Override public boolean isCredentialsNonExpired() { return true; }
+    @Override public boolean isEnabled() { return true; }
+}
