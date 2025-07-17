d119bd47f72133508b7fb856de461eea5aaa8051
Hasegos <csw020106@naver.com>
2025-07-17 11:01:18 +0900

    add 에러 핸들러 추가

---
+++ src/main/java/oreumi/group2/carrotClone/error/ErrorHandler.java
@@ -0,0 +1,22 @@
+package oreumi.group2.carrotClone.error;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(Exception.class)
+    public String handleAny(Exception ex,
+                            HttpServletRequest request,
+                            Model model) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        System.out.println(status);
+
+        model.addAttribute("message", "오류가 발생했습니다. " + ex.getMessage());
+        return "error-page";
+    }
+}
