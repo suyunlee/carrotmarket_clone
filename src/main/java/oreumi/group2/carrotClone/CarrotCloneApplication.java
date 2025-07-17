7f1fbab9592d84bbf609848c095f32246faff9fe
Hasegos <csw020106@naver.com>
2025-07-17 18:51:01 +0900

    modify Gemini 빈 등록

--- src/main/java/oreumi/group2/carrotClone/CarrotCloneApplication.java
+++ src/main/java/oreumi/group2/carrotClone/CarrotCloneApplication.java
@@ -1,9 +1,12 @@
 package oreumi.group2.carrotClone;
 
+import oreumi.group2.carrotClone.Config.GeminiProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(GeminiProperties.class)
 public class CarrotCloneApplication {
 
 	public static void main(String[] args) {
