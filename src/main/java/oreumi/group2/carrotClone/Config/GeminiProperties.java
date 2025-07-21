235e2251309ee1757b4e03bfb3505ff4b6c845c3
Nyppp <wlsdnrla216@naver.com>
2025-07-21 09:09:26 +0900

    add ai prompt

--- src/main/java/oreumi/group2/carrotClone/Config/GeminiProperties.java
+++ src/main/java/oreumi/group2/carrotClone/Config/GeminiProperties.java
@@ -1,14 +1,15 @@
 package oreumi.group2.carrotClone.Config;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "gemini")
 @Data
 public class GeminiProperties {
-
+    @Value("${google.ai.api.key}")
     private String apikey;
-    private String model;
-    private double temperature;
-    private int topK;
+    private String model =  "models/gemini-2.5-flash";
+    private double temperature = 0.7;
+    private int topK = 1;
 }
